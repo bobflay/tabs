16077020a8df1d822ab01586704923d13d659f21
Ibrahim FLEIFEL <bob.fleifel@gmail.com>
2018-07-12 11:03:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+Vue.component('tabs',{
+	template:`
+
+		<div>
+
+			<div class="tabs">
+			  <ul>
+			    <li v-for="tab in tabs" :class="{'is-active':tab.isActive}">
+			    	<a @click="selectTab(tab)">{{tab.title}}</a>
+			    </li>
+			  </ul>
+			</div>
+
+
+			<div class="tabs-detail">
+				<slot></slot>
+			</div>
+
+
+		</div>
+	`,
+	data(){
+
+		return {
+			tabs:[]
+		}
+	},
+	created(){
+		this.tabs = this.$children;
+	},
+	methods:{
+		selectTab(selectedTab){
+			this.tabs.forEach(tab=>{
+				tab.isActive = (tab.title == selectedTab.title);
+
+			});	
+
+
+
+		}
+	}
+
+
+
+});
+
+
+Vue.component('tab',{
+	template:`
+		<div v-show="isActive">
+			<slot></slot>
+		</div>
+	`,
+
+	props:{
+		title:{required:true},
+		selected:{default:false}
+	},
+
+	data(){
+		return {
+			isActive:false
+		}
+	},
+	mounted(){
+		this.isActive = this.selected;		
+	}
+
+
+});
+
+
+
+
+var app = new Vue({
+	el:"#root",
+	data:{
+
+	}
+});
